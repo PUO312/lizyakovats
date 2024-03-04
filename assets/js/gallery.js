f6175a5814a0de2d078b46daec530f4c11cb18c3
PUO312 <tema00757@gmail.com>
2024-03-05 00:20:26 +0200

    done

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,39 +1,33 @@
 $(function(){
-  
     var swiper = new Swiper('.carousel-gallery .swiper-container', {
-      effect: 'slide',
-      speed: 900,
-      slidesPerView: 5,
-      spaceBetween: 20,
-      simulateTouch: true,
-      autoplay: {
-        delay: 5000,
-        stopOnLastSlide: false,
-        disableOnInteraction: false
-      },
-      pagination: {
-        el: '.carousel-gallery .swiper-pagination',
-        clickable: true
-      },
-      breakpoints: {
-        // when window width is <= 320px
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 5
+        effect: 'slide',
+        speed: 900,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        simulateTouch: true,
+        autoplay: false {
+            delay: 5000,
+            stopOnLastSlide: false,
+            disableOnInteraction: false
         },
-        // when window width is <= 480px
-        425: {
-          slidesPerView: 2,
-          spaceBetween: 10
+        pagination: {
+            el: '.carousel-gallery .swiper-pagination',
+            clickable: true
         },
-        // when window width is <= 640px
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 20
+        initialSlide: 0, // Устанавливаем первый слайд
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 5
+            },
+            425: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
         }
-      }
-    }); /*http://idangero.us/swiper/api/*/
-  
-    
-  
+    });
 });
